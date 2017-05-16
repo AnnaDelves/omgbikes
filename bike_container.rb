d5f264886b41fcdbd3348fb0853d2d9f632fcd42
Anna Delves <anna.delves@simplybusiness.co.uk>
2017-05-16 10:43:07 +0100

    Iunno what this stuff is Toby, I did it like a week ago

--- bike_container.rb
+++ bike_container.rb
@@ -10,7 +10,22 @@ module BikeContainer
   end
 
   def add_bike(bike)
+    fail "This #{self.class.name} is full, you doughnut" if full?
     @bikes << bike
+  end
+
+  def remove_bike
+    bikes.pop
+  end
+
+
+  private
+  def full?
+    @bikes.count >= @capacity
+  end
+
+  def empty?
+    @bikes.empty?
   end
 
 end
--- spec/support/shared_examples_for_bike_container.rb
+++ spec/support/shared_examples_for_bike_container.rb
@@ -13,13 +13,33 @@ shared_examples_for BikeContainer do
   end
 
   describe 'add bike' do
-
     it 'is able to recieve a bike' do
       annas_bike = double :bike
       subject.add_bike(annas_bike)
       expect(subject.bikes).to include annas_bike
     end
 
+    it 'raises an error when full' do
+     subject.capacity.times { subject.add_bike double(:bike) }
+     expect { subject.add_bike double(:bike) }.to raise_error "This #{described_class.name} is full, you doughnut"
+   end
+  end
+
+  describe 'remove bike' do
+
+    let(:bike)
+    # Yo you gotta finish this bit
+
+    it 'is able to have bikes removed' do
+      annas_bike = double :bike
+      subject.add_bike(annas_bike)
+      subject.remove_bike
+      expect(subject.bikes).to be_empty
+    end
+
+    it 'checks bike identity' do
+     expect(subject.remove_bike).to eq bike
+   end
   end
 
 end
