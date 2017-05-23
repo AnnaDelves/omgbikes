de17b64295d63498589cdc8866ea4820c350c4dd
Anna Delves <anna.delves@simplybusiness.co.uk>
2017-05-23 14:59:11 +0100

    Making 'release a bike' work again with the bike container module installed in docking station

--- docking_station.rb
+++ docking_station.rb
@@ -3,11 +3,12 @@ class DockingStation
   include BikeContainer
 
   def release_a_bike
-    @docked_bikes.each_with_index do |bike, index|
-      if bike.broken? == false
-        return @docked_bikes.delete_at(index)
+    fail "There are no bikes here you doughnut" if @bikes.empty?
+      @bikes.each_with_index do |bike, index|
+        if bike.broken? == false
+          return @bikes.delete_at(index)
+        end
       end
-    end
     fail "Sorry, all the bikes here are out of order at the moment, please try another docking station"
   end
 
