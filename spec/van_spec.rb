e3463b64182b8caa57fc88b38ec36c53cd2cbc03
Anna Delves <anna.delves@simplybusiness.co.uk>
2017-06-22 11:36:42 +0100

    Adding in the bike container module to vans and changing everything so that it works

--- spec/van_spec.rb
+++ spec/van_spec.rb
@@ -1,55 +1,59 @@
 require './van'
+require './bike_container'
 
 describe Van do
+
+it_behaves_like BikeContainer
+
   before(:each) do
     @workingbike = double(:fakebike, broken?: false)
     @brokenbike = double(:fakebike, broken?: true)
     @dockingstation = double(:fakedockingstation, docked_bikes:[@workingbike, @brokenbike, @brokenbike, @workingbike, @brokenbike], bike_location_type:'container')
-    @garage = double(:fakegarage, broken_bike_bay:[], bike_location_type:'fixer')
-    @emptydockingstation = double(:fakedockingsation, docked_bikes:[], bike_location_type:'container')
-  end
-
-  it 'sets the capacity of the van when initialized' do
-    white_van = Van.new(:small)
-    expect(white_van.capacity).to eq 15
+    @garage = double(:fakegarage, bikes:[])
+    @emptydockingstation = double(:fakedockingsation, bikes:[])
+    allow(@garage).to receive(:class).and_return(Garage)
+    allow(@dockingstation).to receive(:class).and_return(DockingStation)
   end
 
   it 'will collect bikes from the docking station when instructed' do
     gresham_street = @dockingstation
-    white_van = Van.new(:small)
+    white_van = Van.new
     white_van.collect_bikes(gresham_street)
-    expect(white_van.stored_bikes).to eq [@brokenbike, @brokenbike, @brokenbike]
+    expect(white_van.bikes).to eq [@brokenbike, @brokenbike, @brokenbike]
   end
 
   it 'will remove bikes from docking station when collecting them' do
     gresham_street = @dockingstation
-    white_van = Van.new(:small)
+    white_van = Van.new
     white_van.collect_bikes(gresham_street)
     expect(gresham_street.docked_bikes).to eq [@workingbike, @workingbike]
   end
 
   it 'will unload broken bikes when at a garage' do
-      white_van = Van.new(:large)
-      white_van.stored_bikes = [@workingbike, @brokenbike, @brokenbike, @workingbike, @brokenbike]
-      # Toby needs to research how to keep my vans safe
+      white_van = Van.new
+      white_van.add_bike(@workingbike)
+      white_van.add_bike(@brokenbike)
+      white_van.add_bike(@brokenbike)
       white_van.unload(@garage)
-      expect(@garage.broken_bike_bay).to eq [@brokenbike, @brokenbike, @brokenbike]
+      expect(@garage.bikes).to eq [@brokenbike, @brokenbike]
   end
 
   it 'when bikes are unloaded they will be removed from the van' do
-      white_van = Van.new(:large)
-      white_van.stored_bikes = [@workingbike, @brokenbike, @brokenbike, @workingbike, @brokenbike]
-      # Toby still needs to research how to keep my vans safe
+      white_van = Van.new
+      white_van.add_bike(@workingbike)
+      white_van.add_bike(@brokenbike)
+      white_van.add_bike(@brokenbike)
       white_van.unload(@garage)
-      expect(white_van.stored_bikes).to eq [@workingbike, @workingbike]
+      expect(white_van.bikes).to eq [@workingbike]
   end
 
   it 'will unload working bikes when at a docking station' do
-      white_van = Van.new(:large)
-      white_van.stored_bikes = [@workingbike, @brokenbike, @brokenbike, @workingbike, @brokenbike]
-      # Toby needs to research how to keep my vans safe
+      white_van = Van.new
+      white_van.add_bike(@workingbike)
+      white_van.add_bike(@workingbike)
+      white_van.add_bike(@brokenbike)
       white_van.unload(@emptydockingstation)
-      expect(@emptydockingstation.docked_bikes).to eq [@workingbike, @workingbike]
+      expect(@emptydockingstation.bikes).to eq [@workingbike, @workingbike]
   end
 
 end
--- van.rb
+++ van.rb
@@ -1,33 +1,26 @@
 class Van
 
-  attr_reader :capacity
-  attr_accessor :stored_bikes
-
-  def initialize(van_size)
-    @stored_bikes = []
-    @capacity = size_lookup(van_size)
-  end
+  include BikeContainer
 
   def collect_bikes(docking_station)
     docking_station.docked_bikes.delete_if do |bike|
       if bike.broken? == true
-        @stored_bikes << bike
+        @bikes << bike
       end
     end
   end
 
   def unload(bike_dropoff_area)
-    if bike_dropoff_area.bike_location_type == 'fixer'
-      #The fact that garages are fake in the test might be messing things up. Toby is going to think about it. If he hasn't come back with a solution this time next week be sad.
-      @stored_bikes.delete_if do |bike|
+    if bike_dropoff_area.class == Garage
+      @bikes.delete_if do |bike|
         if bike.broken? == true
-          bike_dropoff_area.broken_bike_bay << bike
+          bike_dropoff_area.bikes << bike
         end
       end
     else
-      @stored_bikes.delete_if do |bike|
+      @bikes.delete_if do |bike|
         if bike.broken? == false
-          bike_dropoff_area.docked_bikes << bike
+          bike_dropoff_area.bikes << bike
         end
       end
     end
