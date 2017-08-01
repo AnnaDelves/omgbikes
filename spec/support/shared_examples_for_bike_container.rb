034a74b745052e21736abb4b1c15fec6be061b36
Anna Delves <anna.delves@simplybusiness.co.uk>
2017-08-01 12:05:48 +0100

    Refactoring tests for bike container module to remove 'subject'

--- spec/support/shared_examples_for_bike_container.rb
+++ spec/support/shared_examples_for_bike_container.rb
@@ -1,8 +1,10 @@
 shared_examples_for BikeContainer do
 
+  let(:bikecontainer) {described_class.new}
+
   describe 'capacity' do
     it 'has a default capacity' do
-      expect(subject.capacity).to eq BikeContainer::DEFAULT_CAPACITY
+      expect(bikecontainer.capacity).to eq BikeContainer::DEFAULT_CAPACITY
     end
 
     it "can have it's capacity set to something other than the default at initialisation" do
@@ -15,13 +17,13 @@ shared_examples_for BikeContainer do
   describe 'add bike' do
     it 'is able to recieve a bike' do
       annas_bike = double :bike
-      subject.add_bike(annas_bike)
-      expect(subject.bikes).to include annas_bike
+      bikecontainer.add_bike(annas_bike)
+      expect(bikecontainer.bikes).to include annas_bike
     end
 
     it 'raises an error when full' do
-     subject.capacity.times { subject.add_bike double(:bike) }
-     expect { subject.add_bike double(:bike) }.to raise_error "This #{described_class.name} is full, you doughnut"
+     bikecontainer.capacity.times { bikecontainer.add_bike double(:bike) }
+     expect { bikecontainer.add_bike double(:bike) }.to raise_error "This #{described_class.name} is full, you doughnut"
    end
   end
 
@@ -32,16 +34,16 @@ shared_examples_for BikeContainer do
       let(:bike) {double :bike}
 
       before(:each) do
-        subject.add_bike(bike)
+        bikecontainer.add_bike(bike)
       end
 
       it 'is able to have bikes removed' do
-        subject.remove_bike
-        expect(subject.bikes).to be_empty
+        bikecontainer.remove_bike
+        expect(bikecontainer.bikes).to be_empty
       end
 
       it 'checks bike identity' do
-       expect(subject.remove_bike).to eq bike
+       expect(bikecontainer.remove_bike).to eq bike
      end
 
    end
@@ -49,7 +51,7 @@ shared_examples_for BikeContainer do
    context 'when there are no bikes' do
 
      it 'raises an error when empty' do
-       expect { expect(subject.remove_bike) }.to raise_error "This #{described_class.name} is empty, you doughnut"
+       expect { expect(bikecontainer.remove_bike) }.to raise_error "This #{described_class.name} is empty, you doughnut"
      end
 
     end
